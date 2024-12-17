07dbe411eb3587fd70d0ca03e5e3a0bdec04f891
nouran250 <nouran.abdelsamei@ejust.edu.eg>
2024-12-17 18:53:51 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App() {
+  return (
+      <div>
+        {/* Other content */}
+        <h1>Elite Care</h1>
+        <p>Project advanced</p>
+      </div>
+  );
+}
+
+export default App;
+
